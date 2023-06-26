ce5f1c4a30222b9532c02b7fed1a0dfb55584d3a
Kholodetskiy <sergh.yanovskiy@inbox.ru>
2023-06-26 21:44:48 +0500

    add new game

---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import gameLogic from '../index.js';
+import getRandomNumber from '../getRandomInt.js'
+
+const prime = () => {
+  const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+  const taskPrime = () => {
+    const question = getRandomNumber() + 1;
+    let answer;
+    for (let i = 2; i < question; i += 1) {
+      if ((question % i === 0) || (question === 1)) {
+        answer = 'no';
+        break;
+      } else {
+        answer = 'yes';
+      }
+    }
+    return [question, answer];
+  };
+  gameLogic(rule, taskPrime);
+};
+export default prime;
